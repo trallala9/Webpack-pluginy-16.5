aaf1ece190ecb2b1e5287cdc6f4893f9bf3cfd21
Karolina Siwkowska <trallala00@gmail.com>
2018-03-19 23:45:16 +0100

    Init project

---
+++ webpack.config.js
@@ -0,0 +1,38 @@
+const path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var UglifyJSPlugin = require('uglifyjs-webpack-plugin');
+//webpack.config.js
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                include: /src/,
+                loader: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                include: /css/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
+
+};
